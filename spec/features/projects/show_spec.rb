c3b8a422138918cacb4df12e924c1e6ab347e2d0
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-05 19:08:08 -0600

    feat: completed user story 2 with all tests passing

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -6,16 +6,8 @@ RSpec.describe 'the projects show page' do
         @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
     end
 
-
-# As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
     it 'shows the projects name and material, along with the challenge theme on the projects#show page' do
-        visit "/projects/#{@lit_fit.id}" #might need to revisit which id 
+        visit "/projects/#{@lit_fit.id}" 
 
         expect(page).to have_content(@lit_fit.name)
         expect(page).to have_content(@lit_fit.material)
